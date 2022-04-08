85191b1a7dab4586cc7d2e07891d8a7368816343
prabagarc@gmail.com <prabagarc@gmail.com>
2022-04-08 13:12:51 +0530

    first commit

---
+++ samplebootapp/src/main/java/com/baggu/samplebootapp/SamplebootappApplication.java
@@ -0,0 +1,13 @@
+package com.baggu.samplebootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplebootappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplebootappApplication.class, args);
+	}
+
+}
---
+++ samplebootapp/src/test/java/com/baggu/samplebootapp/SamplebootappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baggu.samplebootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplebootappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
